7ccae2339df5fdb45f22cd976262e43fc0b1a364
martin-langa <martinelverolanga@gmail.com>
2026-08-08 18:58:55 +0200

    feat: adicionado o modulo de signup e config da db

---
+++ prisma/migrations/20260808164403_initial_schema/migration.sql
@@ -0,0 +1,47 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/controllers/auth-controller.ts
@@ -0,0 +1,62 @@
+import type { Request, Response } from "express";
+import z from "zod";
+import prisma from "../util/db.js";
+import { hash } from "bcrypt";
+
+class AuthController {
+
+    signIn = async (req: Request, res: Response) => {
+
+        try{
+            const bodySchema = z.object({
+                username: z.string().min(3, "Username must have 3 or more letters"),
+                email: z
+                    .email()
+                    .includes("@")
+                    .trim(),
+                password: z
+                    .string()
+                    .min(6, "Password must be at least 6 characters long")
+            });
+
+            const { username, email, password } = bodySchema.parse(req.body);
+
+            const isRegistered = await prisma.user.findFirst({
+                where: { email }
+            });
+
+            if(isRegistered){
+                return res.status(409).json({
+                    success: false,
+                    message: "O email já está em uso"
+                });
+            }
+
+            const passwordHash = await hash(password, 10);
+
+            const newUser = await prisma.user.create({
+                data: {
+                    email,
+                    passwordHash,
+                    username
+                }
+            });
+
+
+            return res.status(201).json({
+                success: true,
+                message: "User registered successfully"
+            });
+        }catch(error){
+            return res.status(500).json({
+                success: false,
+                message: "Ocorreu um erro no modulo de registro",
+                error: error
+            });
+        }
+    }
+    
+    signUp = async (req: Request, res: Response) => {
+
+    }
+}
